c0be2d95b801ec2826cb54a37683614ed3818a0e
Pedro Castro <up201605469@fe.up.pt>
2020-12-03 14:59:23 +0000

    Estruturação em classe

--- data_monitor/excel_extract.py
+++ data_monitor/excel_extract.py
@@ -3,35 +3,85 @@ import time
 import pandas as pd
 import numpy as np
 
-def filter_DGA(file_path):
-	df = pd.read_excel(file_path, sheet_name='DGA')
-	df.drop(df.columns[[0,1,2]], axis=1, inplace=True)
-	df.drop(df.index[0], axis=0, inplace=True)
-	
-	return df
+class Excel_extract:
+	def __init__(self,file_path):
+		self.file_path = file_path
 
+	def filter_DGA(self):
+		df = pd.read_excel(self.file_path, sheet_name='DGA')
+		df.drop(df.columns[[0,1,2]], axis=1, inplace=True)
+		df.drop(df.index[0], axis=0, inplace=True)
 
-def filter_DGAF(file_path):
-	df = pd.read_excel(file_path, sheet_name='DGAF')
-	number_of_tables = len(df.index)
+		return df
 
-	df_DGAF_scores = []
-	df_DGAF_ratings = []
-	for j in range(0, 12, 11): # Para percorrer as tabelas na horizontal
-		for i in range(0, number_of_tables, 15): # Para percorrer as tabelas na vertical
-			df_score = df.iloc[i:i+7,j:j+9]
-			df_score.drop(df.columns[[j+0,j+1,j+5,j+7]], axis=1, inplace=True) # É possível retirar este conteudo com o método de cima, mas preferi que ficasse explicito que largamos os nomes
-			
-			# Apenas para não inserir tabelas sem conteudo, um pouco trolha, só vê o primeiro valor da tabela
-			if df_score.iloc[0,0] is np.nan: break
+	def filter_DGAF(self):
+		df = pd.read_excel(self.file_path, sheet_name='DGAF')
+		number_of_tables = len(df.index)
 
-			df_DGAF_scores.append(df_score)
+		df_DGAF_scores = []
+		df_DGAF_ratings = []
+		for j in range(0, 12, 11): # Para percorrer as tabelas na horizontal
+			for i in range(0, number_of_tables, 15): # Para percorrer as tabelas na vertical
+				df_score = df.iloc[i:i+7,j:j+9]
+				df_score.drop(df.columns[[j+0,j+1,j+5,j+7]], axis=1, inplace=True) # É possível retirar este conteudo com o método de cima, mas preferi que ficasse explicito que largamos os nomes
 
-			df_rating = df.iloc[i+9,j:j+2] # Caso se queira usar a tabela com o rating (O naming do dataframe está mal)
-			df_DGAF_ratings.append(df_rating)
+				# Apenas para não inserir tabelas sem conteudo, um pouco trolha, só vê o primeiro valor da tabela
+				if df_score.iloc[0,0] is np.nan: break
 
-	return df_DGAF_scores, df_DGAF_ratings
+				df_DGAF_scores.append(df_score)
+
+				df_rating = df.iloc[i+9,j:j+2] # Caso se queira usar a tabela com o rating (O naming do dataframe está mal)
+				df_DGAF_ratings.append(df_rating)
+
+		return df_DGAF_scores, df_DGAF_ratings
+
+	def filter_FAL(self):
+		df = pd.read_excel(self.file_path, sheet_name='2-FAL')
+		df.drop(df.columns[4:], axis=1, inplace=True)
+		df.drop(df.index[[0,1]], axis=0, inplace=True)
+
+		return df
+
+	def filter_PF(self):
+		df = pd.read_excel(self.file_path, sheet_name='PF')
+		df.drop(df.columns[2:], axis=1, inplace=True)
+
+		df_rating = df.iloc[2,:2]
+		df_samples = df.iloc[5:,:2]
+
+		return df_samples, df_rating
+
+	def filter_GOT(self):
+		df = pd.read_excel(self.file_path, sheet_name='GOT')
+		df.drop(df.columns[0], axis=1, inplace=True)
+		df.drop(df.index[0], axis=0, inplace=True)
+
+		return df
+
+	def filter_OQF(self):
+		pass
+
+	def filter_Load(self):
+		pass
+
+	def filter_Maintenance(self):
+		pass
+
+	def filter_OverallCondition(self):
+		pass
+
+	def filter_FinalCalculation(self):
+		pass
 
 if __name__ == "__main__":	
-	df_DGA = filter_DGA(r'dados/Template SE1.xlsx')
-	df_DGAF,_ = filter_DGAF(r'dados/Template SE1.xlsx') # O _ serve para ignorar o segundo retorno da função
+	
+	ee = Excel_extract(r'dados/Template SE1.xlsx')
+	# df_DGA = filter_DGA(r'dados/Template SE1.xlsx')
+	# df_DGAF,_ = filter_DGAF(r'dados/Template SE1.xlsx') # O _ serve para ignorar o segundo retorno da função
+	# df = filter_FAL(r'dados/Template SE1.xlsx')
+	# df_s, df_r = filter_PF(r'dados/Template SE1.xlsx')
+	# df = filter_GOT(r'dados/Template SE1.xlsx')
+
+	df = ee.filter_GOT()
+
+	print (df)
