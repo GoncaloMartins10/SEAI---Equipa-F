ba86079e7f8cef12b2634f96f9ff226cb7bab16d
up201606808 <up201606808@fe.up.pt>
2020-12-27 17:21:33 +0000

    adaptação das classes measurements às novas tables

--- server/resources/measurements/dissolved_gases/Models.py
+++ server/resources/measurements/dissolved_gases/Models.py
@@ -1,6 +1,6 @@
 from resources import Base
 from resources.Mixins import MixinsTables,MixinsTablesMeasurements
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, Date
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Date, Text
 from sqlalchemy.orm import relationship
 
 class Dissolved_Gases(Base):
@@ -8,15 +8,15 @@ class Dissolved_Gases(Base):
     __table_args__ ={"schema": "ges_ativos"}
 
     id_dissolved_gas_measurement = Column(Integer, primary_key=True)
-    id_transformer  = Column(Integer, ForeignKey('ges_ativos.transformer.id_transformer'))
-    H2 = Column(Float)
-    CH4 = Column(Float)
-    C2H6 = Column(Float)
-    C2H4 = Column(Float)
-    C2H2 = Column(Float)
-    CO = Column(Float)
-    COH2 = Column(Float)
-    timestamp_dissolved_gases_measurements = Column(Date)
+    id_transformer  = Column(Text, ForeignKey('ges_ativos.transformer.id_transformer'))
+    h2 = Column(Float)
+    ch4 = Column(Float)
+    c2h6 = Column(Float)
+    c2h4 = Column(Float)
+    c2h2 = Column(Float)
+    co = Column(Float)
+    cOh2 = Column(Float)
+    #timestamp_dissolved_gases_measurements = Column(Date)
 
     transformer = relationship("Transformer", back_populates="dissolved_gases")
 
