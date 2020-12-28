843424ae1812a86ca838ccec83544bf65490f25e
landaudiogo <diogo.hewitt.landau@hotmail.com>
2020-12-28 15:15:46 +0000

    script de popular db inicial [test_db.py]

--- server/resources/db_classes.py
+++ server/resources/db_classes.py
@@ -1,5 +1,4 @@
 from .transformer.Models import Transformer
-#from .transformer_algorithm_weights.Models import Transformer_Algorithm_Weights
 from .weights.Models import Weights
 from .measurements.furfural.Models import Furfural
 from .measurements.load.Models import Load
