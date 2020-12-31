751156af30c2e75fbd6aef6b9700f15140ec7b07
Pedro Castro <up201605469@fe.up.pt>
2020-12-31 18:49:52 +0000

    Populates the database, only missing maintenance

--- data_monitor/populate.py
+++ data_monitor/populate.py
@@ -14,7 +14,7 @@ from resources.db_classes import Transformer, Furfural, Oil_Quality, Load, Disso
 from resources import Session
 from resources.Mixins import MixinsTables
 
-def convertToFloat(value):
+def _convert_to_float(value):
 	if type(value) is str:
 		filtered = value.split("x") # Não consigo filtrar o ^ por algum motivo...
 		i = float(filtered[0].replace(',','.'))
@@ -29,7 +29,7 @@ def convertToFloat(value):
 		# Não levanta excessão, apenas devolve o valor (meio trolha eu sei)
 		return value
 
-def extract_transformer_ID(transformer: str):
+def _extract_transformer_ID(transformer: str):
 	substation = re.search(r"SE\d\d?", transformer)
 	is_TP = re.search(r"TP\d\d?", transformer)
 	if is_TP: # If it starts or ends with SE_ return id, if it has TP put it in the id
@@ -38,48 +38,48 @@ def extract_transformer_ID(transformer: str):
 		return substation.group(0)
 
 
-def insert_DGA(transformer, dga):
+def _parse_data_to_object_DGA(transformer, dga):
 	samples = []
 	for _, data in dga.iteritems():
 		a = []
 		for d in data:
-			a.append(convertToFloat(d))
-		# sample = Dissolved_gas_measurements(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7])
+			a.append(_convert_to_float(d))
+
 		samples.append(Dissolved_Gases(id_transformer=transformer, datestamp = a[0], h2 = a[1], co = a[2], coh2 = a[3], ch4 = a[4], c2h4 = a[5], c2h6 = a[6], c2h2 = a[7]))
 	return samples
 
-def insert_FAL(transformer, fal):
+def _parse_data_to_object_FAL(transformer, fal):
 	samples = []
 	for _, data in fal.iterrows():
 		a = []
 		for d in data:
 			if type(d) is int or type(d) is float:
-				d = convertToFloat(d)
+				d = _convert_to_float(d)
 			a.append(d)
 		samples.append(Furfural(id_transformer=transformer, datestamp = a[0], quantity = a[1]))
 	
 	return samples
 
-def insert_GOT(transformer, got):
+def _parse_data_to_object_GOT(transformer, got):
 	samples = []
 	for _, data in got.iteritems():
 		a = []
 		for d in data:
-			a.append(convertToFloat(d))
+			a.append(_convert_to_float(d))
 		
 		sample = Oil_quality_measurements(a[0], a[1], a[2], a[3], a[4], a[5])
 		samples.append(Oil_Quality(id_transformer=transformer, datestamp = a[0], breakdown_voltage = a[1], water_content = a[2], acidity = a[3], color = a[4], interfacial_tension = a[5]))
 	return samples
 
 # https://stackoverflow.com/questions/42950/how-to-get-the-last-day-of-the-month
-def last_day_of_month(any_day):
+def _last_day_of_month(any_day):
 	# this will never fail
 	# get close to the end of the month for any day, and add 4 days 'over'
 	next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
 	# subtract the number of remaining 'overage' days to get last day of current month, or said programattically said, the previous day of the first of next month
 	return next_month - datetime.timedelta(days=next_month.day)
 
-def insert_Load(transformer: str, load, Sb):
+def _parse_data_to_object_Load(transformer: str, load, Sb):
 	months = {"jan": 1, "fev" : 2, "mar": 3, "abril": 4, "maio": 5, "jun": 6, "jul": 7, "agosto": 8, "set": 9, "out": 10, "nov": 11, "dez": 12}
 	
 	substation = re.search(r"SE\d?", transformer)
@@ -93,13 +93,13 @@ def insert_Load(transformer: str, load, Sb):
 
 		if year != data.iloc[0] and not np.isnan(data.iloc[0]):
 			year = int(data.iloc[0])
-		timestamp = last_day_of_month(datetime.date(year, months[data.iloc[1]], 1))
-		samples.append(Load(id_transformer= transformer, datestamp= timestamp, load_factor= data[substation]))
+		timestamp = _last_day_of_month(datetime.date(year, months[data.iloc[1]], 1))
+		samples.append(Load(id_transformer= transformer, datestamp= timestamp, load_factor= data[substation], power_factor = 1))
 
 	return samples
 
 
-def insert_Maintenance(transformer: str, maintenance):
+def _parse_data_to_object_Maintenance(transformer: str, maintenance):
 	transformer_voltage = None
 
 	has_descriptive_datetime = (len(maintenance.columns)) > 2
@@ -137,6 +137,9 @@ def insert_Maintenance(transformer: str, maintenance):
 	return samples, transformer_voltage
 		
 def populate_database():
+	"""
+	Populates the database with all the excel files 
+	"""
 	excel_parent_path = "dados"
 	session = Session()
 
@@ -144,7 +147,7 @@ def populate_database():
 
 	for _, transformer in enumerate(transformers):
 		t = Excel_extract(transformer)
-		ID = extract_transformer_ID(transformer)
+		ID = _extract_transformer_ID(transformer)
 
 		load, Sb, _ = t.filter_Load()
 		dga = t.filter_DGA()
@@ -152,15 +155,15 @@ def populate_database():
 		fal = t.filter_FAL()
 		_, _, maintenance = t.filter_Maintenance()
 
-		dga_samples = insert_DGA(ID, dga)
-		fal_samples = insert_FAL(ID, fal)
-		got_samples = insert_GOT(ID, got)
-		load_samples = insert_Load(ID, load, Sb)
-		maint_samples, rated_voltage = insert_Maintenance(ID, maintenance)
+		dga_samples = _parse_data_to_object_DGA(ID, dga)
+		fal_samples = _parse_data_to_object_FAL(ID, fal)
+		got_samples = _parse_data_to_object_GOT(ID, got)
+		load_samples = _parse_data_to_object_Load(ID, load, Sb)
+		maint_samples, rated_voltage = _parse_data_to_object_Maintenance(ID, maintenance)
 
 		# First, insert the transformer in the database because of the foreign key constraint
 		trans = Transformer(id_transformer= ID, nominal_voltage= rated_voltage)
-		MixinsTables.add_batch(session, trans)
+		trans.add(session)
 
 		MixinsTables.add_batch(session, dga_samples)
 		MixinsTables.add_batch(session, fal_samples)
@@ -168,8 +171,12 @@ def populate_database():
 		MixinsTables.add_batch(session, load_samples)
 		# MixinsTables.add_batch(session, maint_samples)
 
+	session.close()
 
 if __name__ == "__main__":
+
+	populate_database()
+
 	excel_parent_path = "dados"
 	db_select = "docker"
 	if db_select is "docker":
@@ -188,47 +195,3 @@ if __name__ == "__main__":
 			'database':'seai'}
 	else:
 		raise DatabaseException("No database selected, database " + db_select + " none existent")
-	
-	# session = Session()
-
-	# db = DataBase(db_url,False)
-
-	# transformer_1 = Excel_extract(r'dados/Template SE1.xlsx')
-
-	# ver glob.glob (permite fazer regex)
-	transformers = os.listdir(excel_parent_path)
-
-
-	print(transformers)
-
-	
-	transf = glob.glob(os.path.join(excel_parent_path,"*.xlsx"))
-
-	print(transf)
-
-	for number, transformer in enumerate(transformers):
-		t = Excel_extract(os.path.join(excel_parent_path,transformer))
-		ID = extract_transformer_ID(transformer)
-
-		# load, Sb, _ = t.filter_Load()
-		# load_samples = insert_Load(ID, load, Sb)
-
-		dga = t.filter_DGA()
-		dga_samples = insert_DGA(ID, dga)
-
-		# fal = t.filter_FAL()
-		# fal_samples = insert_FAL(ID, fal)
-
-		# got = t.filter_GOT()
-		# got_samples = insert_GOT(ID, got)
-
-		# _, _, maintenance = t.filter_Maintenance()
-
-		# insert_DGA(db, dga)
-		# insert_GOT(db, got)
-		# maint_samples, rated_voltage = insert_Maintenance(db, maintenance)
-
-		#trans = [Transformer(id_transformer= ID, nominal_voltage= 200)]
-		#MixinsTables.add_batch(session, trans)
-		
-		# MixinsTables.add_batch(session, load_samples)
