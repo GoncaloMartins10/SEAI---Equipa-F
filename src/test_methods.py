c0542ad31b4c6bd1b6a46e157f85f351f5b9b427
Pedro Castro <up201605469@fe.up.pt>
2021-01-04 12:48:40 +0000

    Algoritmo 4 completo

--- src/test_methods.py
+++ src/test_methods.py
@@ -2,4 +2,11 @@ from imports.HI_calculation.method_4 import *
 from imports.resources import Session
 
 session = Session()
-test(session)
+calculate_all_transformers(session)
+
+#populate_method_4()
+#n = Method_4(100)
+
+#r, s = n.calc_dga(0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2)
+
+#print(r, s)
