9de6e151945b576758da4385f0803b0fa4f1b900
Danielbtw <up201708982@fe.up.pt>
2021-01-16 17:00:41 +0000

    HI_combinado normalizado

--- src/imports/HI_calculation/algorithms/method_3.py
+++ src/imports/HI_calculation/algorithms/method_3.py
@@ -43,9 +43,10 @@ class MultiFeatureIndex(Method):
 		else:
 			self.hi_oil = self.get_oil(data[2])
 
-		HI_combinado = self.get_combinado(self.hi_main, self.hi_iso, self.hi_dga, self.hi_oil)
+		HI_combinado = self.get_combinado(self.hi_main, self.hi_iso, self.hi_dga, self.hi_oil) 
+		HI_combinado_norm = HI_combinado * 10
 
-		return HI_combinado 
+		return HI_combinado_norm
 
 	def get_combinado(self, hi_main, hi_iso, hi_dga, hi_oil):
 		w=[self.pesos_combinado["HI_m"], self.pesos_combinado["HI_iso"], self.pesos_combinado["HI_CH"], self.pesos_combinado["HI_oil"]]
