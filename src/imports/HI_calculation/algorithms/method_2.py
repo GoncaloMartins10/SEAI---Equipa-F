43457ee61fd964886002e8ab34e7053fecc8fd8b
landaudiogo <diogo.hewitt.landau@hotmail.com>
2021-01-17 13:47:08 +0000

    added merge from Pedro to landau_test

--- src/imports/HI_calculation/algorithms/method_2.py
+++ src/imports/HI_calculation/algorithms/method_2.py
@@ -13,6 +13,8 @@ class Method_2(Method):
 		if transformer:
 			if transformer.nominal_voltage:
 				self.update_method_weights(transformer.nominal_voltage)
+			else:
+				self.update_method_weights(None)
 
 	def __repr__(self):
 		return f'Method 2'
@@ -44,33 +46,34 @@ class Method_2(Method):
 
 				self.dga = WS(self.config[w]["Dissolved_Gases"], val[w][s], val[w][sc])
 			elif key == "Oil_Quality":
-				if transformer_voltage is not None:
-					for k, v in val.items():
-						if "voltage" in v:
-							rated_volts = list(map(int, list(v["voltage"].keys())))
-							rated_volts.sort(reverse = True)
-							for volts in rated_volts:
-								if transformer_voltage >= volts:
-									weight = WS(v[w], v["voltage"][str(volts)][s], v["voltage"][str(volts)][sc])
-									break
-						else:
-							if k == "weight":
-								continue
-							weight = WS(v[w], v[s], v[sc])
-						if k == "Dielectric_strength":
-							self.breakdown_voltage = weight
-						elif k == "Interfacial_tension":
-							self.interfacial_tension = weight
-						elif k == "Acidity":
-							self.acidity = weight
-						elif k == "Water_content":
-							self.water_content = weight
-						elif k == "Color":
-							self.color = weight
-						elif k == "Dissipation_factor":
-							self.dissipation_factor = weight
-						else:
-							raise Exception
+				if transformer_voltage is None or isnan(transformer_voltage): transformer_voltage = 220
+
+				for k, v in val.items():
+					if "voltage" in v:
+						rated_volts = list(map(int, list(v["voltage"].keys())))
+						rated_volts.sort(reverse = True)
+						for volts in rated_volts:
+							if transformer_voltage >= volts:
+								weight = WS(v[w], v["voltage"][str(volts)][s], v["voltage"][str(volts)][sc])
+								break
+					else:
+						if k == "weight":
+							continue
+						weight = WS(v[w], v[s], v[sc])
+					if k == "Dielectric_strength":
+						self.breakdown_voltage = weight
+					elif k == "Interfacial_tension":
+						self.interfacial_tension = weight
+					elif k == "Acidity":
+						self.acidity = weight
+					elif k == "Water_content":
+						self.water_content = weight
+					elif k == "Color":
+						self.color = weight
+					elif k == "Dissipation_factor":
+						self.dissipation_factor = weight
+					else:
+						raise Exception
 
 				self.got = WS(self.config[w]["Oil_Quality"], val[w][s], val[w][sc])
 			elif key == "Furfural":
